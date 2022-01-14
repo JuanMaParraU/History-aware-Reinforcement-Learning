4cf158885480cbe28edba28145a56b5c7bc1c2f9
JuanMaParraU <j.parra-ullauri@aston.ac.uk>
2022-01-14 10:35:28 +0000

    Message received analyis for stopping the update of the target network

--- uk.ac.aston.drones.source.dqn/package/main.py
+++ uk.ac.aston.drones.source.dqn/package/main.py
@@ -250,7 +250,7 @@ def on_message(client, userdata, msg):
     isMessageReceived = True
     global message 
     message = str(msg.payload)
-    print(msg.topic+" "+str(msg.payload))
+    #print(msg.topic+" "+str(msg.payload))
 
 def on_log(client, userdata, level, buf):
     global log
