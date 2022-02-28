7c348681d485eef61aced59d83f27c0bd2e72d3d
Juan Marcelo Parra <170230317@aston.ac.uk>
2022-02-28 14:05:57 +0000

    q_learining updated

--- uk.ac.aston.drones.source.qlearning/main.py
+++ uk.ac.aston.drones.source.qlearning/main.py
@@ -3,49 +3,49 @@ import math
 import json
 from json import JSONEncoder
 import torch
-#import pymongo
 import argparse
 import random
 import models
 from models import Q_Learning
 import pandas as pd
-#from pymongo import MongoClient
-from pandas import DataFrame,Series
+from pandas import DataFrame, Series
 import matplotlib.pyplot as plt, time
 from matplotlib.patches import Circle
 from datetime import datetime
 import pickle
 import copy
 import paho.mqtt.client as mqtt
-from paho.mqtt.subscribe import _on_connect
-
+#=======================================================================================================================
+# Environment Parameters
 parser = argparse.ArgumentParser(description='Reinforce Learning')
 parser.add_argument('--random_seed', default=19, type=int, help='The specific seed to generate the random numbers')
-parser.add_argument('--numDrones', default=2, type=int, help='The number of Drones(UAV)')
+parser.add_argument('--numDrones', default=4, type=int, help='The number of Drones(UAV)')
 parser.add_argument('--numUsers', default=1050, type=int, help='The number of Users')
 parser.add_argument('--length', default=100, type=int, help='The length of the area(meter)')
 parser.add_argument('--width', default=100, type=int, help='The width of the area(meter)')
 parser.add_argument('--resolution', default=10, type=int, help='The Resolution (meter)')
-parser.add_argument('--episode', default=2, type=int, help='The number turns it plays')
+parser.add_argument('--episode', default=100, type=int, help='The number turns it plays')
 parser.add_argument('--step', default=1000, type=int, help='The number of steps for any turn of runs')
-parser.add_argument('--action_space', default=['east','west','south','north','stay'], type=list, help='The avaliable states')
+parser.add_argument('--action_space', default=['east', 'west', 'south', 'north', 'stay'], type=list,
+                    help='The avaliable states')
 parser.add_argument('--EPSILON', default=0.9, type=float, help='The greedy policy')
 parser.add_argument('--ALPHA', default=0.3, type=float, help='The learning rate')
 parser.add_argument('--LAMBDA', default=0.9, type=float, help='The discount factor')
-
+#=======================================================================================================================
+# Signal Attenuation Parameters
 parser.add_argument('--connectThresh', default=40, type=int, help='Threshold')
 parser.add_argument('--dAngle', default=60, type=int, help='The directivity angle')
 parser.add_argument('--fc', default=2.4e9, type=int, help='The carrier frequency')
 parser.add_argument('--Pt', default=0, type=int, help='The drone transmit power in Watts')
 parser.add_argument('--BW', default=200e3, type=int, help='The bandwidth')
-parser.add_argument('--N0', default=10**(-20.4), type=float, help='The N0')
+parser.add_argument('--N0', default=10 ** (-20.4), type=float, help='The N0')
 parser.add_argument('--SIGMA', default=20, type=int, help='The SIGMA')
-
-parser.add_argument('--database_name', default='Q_Learning_Data_Base', type=str, help='The name of database')
-parser.add_argument('--collection_name', default='Q_table_collection', type=str, help='The name of the collection')
-parser.add_argument('--host', default='localhost', type=str, help='The host type')
-parser.add_argument('--mongodb_port', default=27017, type=int, help='The port of database')
-
+#=======================================================================================================================
+# Mqtt Parameters
+parser.add_argument('--mqttBroker', default='broker.mqttdashboard.com', type=str, help='The mqtt broker url, e.g 127.0.0.1')
+parser.add_argument('--q_table_topic', default='Q_table_collection.json', type=str, help='The name of the main topic')
+parser.add_argument('--port', default=1883, type=int, help='The mqtt port')
+parser.add_argument('--initial_param_topic', default='initial_setting.json', type=str, help='The name of the topic for the initial parameters')
 
 args = parser.parse_args()
 Q = Q_Learning(args)
@@ -57,31 +57,26 @@ def generate_dict_from_array(array, name):
         for j in range(np.shape(array)[1]):
             if j != 0:
                 data += ","
-            data += str(array[i,j])
+            data += str(array[i, j])
         data += " )"
-        dict [name + " " + str(i)] = data
+        dict[name + " " + str(i)] = data
     return copy.deepcopy(dict)
 
 
-
-def refreash_dataset(name = args.database_name, collection_name = args.collection_name, host='localhost', port=27017):
-    mongo_client = MongoClient(host, port) # 创建 MongoClient 对象，（string格式，int格式）
-    mongo_db = mongo_client[name] # MongoDB 中可存在多个数据库，根据数据库名称获取数据库对象（Database）
-    #mongo_db.authenticate(mongodb_user, mongodb_passwd) # 登录认证
-    for collection in mongo_db.collection_names():
-        print ('collection: ', collection, ' have been refreshed')
-        db_collection=mongo_db[collection] # 每个数据库包含多个集合，根据集合名称获取集合对象（Collection）
-        # drop = db_collection.drop() delate
-        remove = db_collection.remove()
-        #drop = db_collection.drop()
-
 def environment_setup():
     np.random.seed(args.random_seed)
-    u = np.random.randint(300,700)
+    u = np.random.randint(300, 700)
     # dronePos = np.zeros((args.numDrones,3))
     # dronePos[:,0:2] = np.random.randint(0, int(args.length/args.resolution),[args.numDrones,2])*10+5
     # dronePos[:,2] = 30
-    dronePos = np.array([[0, 0, 30], [99, 99, 30]])
+    if args.numDrones == 2:
+        dronePos = np.array([[5, 5, 30], [95, 95, 30]])
+        # dronePos = np.array([[5, 5, 30], [5, 5, 30]])
+    elif args.numDrones == 4:
+        dronePos = np.array([[5, 5, 30], [95, 95, 30], [5, 95, 30], [95, 5, 30]])
+    elif args.numDrones == 8:
+        dronePos = np.array(
+            [[5, 5, 30], [95, 95, 30], [5, 95, 30], [95, 5, 30], [5, 50, 30], [50, 5, 30], [95, 50, 30], [50, 95, 30]])
 
     # userPos = np.zeros((args.numUsers,3))
     # userPos[:,0:2] =np.floor((np.random.randn(args.numUsers,2)*args.SIGMA*5 + u)%args.length)
@@ -139,34 +134,49 @@ def environment_setup():
             userPos = cluster[dict]
         else:
             userPos = np.concatenate((userPos, cluster[dict]), axis=0)
-    
+
     userPos[:, 2] = 1.5
     user_x_y = userPos
-    
-    userPos_XY={}
+
+    userPos_XY = {}
     for i in range(np.shape(user_x_y)[0]):
-        pos=userPos_XY[str(i)]= {}
+        pos = userPos_XY[str(i)] = {}
         for j in range(np.shape(user_x_y)[1]):
-            if j == 0: 
-                pos["x"]=str(user_x_y[i,j])
-            else: 
+            if j == 0:
+                pos["x"] = str(user_x_y[i, j])
+            else:
                 if j == 1:
-                    pos["y"]=str(user_x_y[i,j])   
-    #save_initial_settling(userPos,dronePos)
-    save_initial_settings_mqtt(userPos, dronePos,userPos_XY)
+                    pos["y"] = str(user_x_y[i, j])
+                    # save_initial_settling(userPos,dronePos)
+    save_initial_settings_mqtt(userPos, dronePos, userPos_XY)
     Q_table = {}
     for i in range(args.numDrones):
         Q_table[i] = Q.build_Q_table()
     return dronePos, userPos, Q_table
 
+
 ##MQTT
 def on_connect(client, userdata, flags, rc):
     print('CONNACK received with code %d.' % (rc))
-    
-def save_initial_settings_mqtt(U_p, D_p, userPos_XY, name = args.database_name, topic_name ='initial_setting.json', host='broker.hivemq.com', port=1883):
-    mqttClient=mqtt.Client()
+
+def on_message(client, userdata, msg):
+    global isMessageReceived
+    isMessageReceived = True
+    global message
+    message = str(msg.payload)
+    #print(msg.topic+" "+str(msg.payload))
+
+def on_log(client, userdata, level, buf):
+    global log
+    log = buf
+
+def save_initial_settings_mqtt(U_p, D_p, userPos_XY, topic_name =args.initial_param_topic, host=args.mqttBroker, port=args.port):
+    mqttClient=mqtt.Client(client_id="ETeMoXServer")
     mqttClient.on_connect = on_connect
+    mqttClient.on_log = on_log
     mqttClient.connect(host, port)
+    print(host,port)
+    mqttClient.loop_start()
     initial_info = {}
     initial_info ['random_seed'] = args.random_seed
     initial_info ['num_drones'] = args.numDrones
@@ -187,102 +197,63 @@ def save_initial_settings_mqtt(U_p, D_p, userPos_XY, name = args.database_name, 
     initial_info ['iterations_per_episode'] = args.step
     initial_info ['discount_factor'] = args.LAMBDA
     initial_info ['episodes'] = 'total if possible'
+    print('here')
     mqttClient.publish(topic_name, str(initial_info))
     userPos_XY = str(userPos_XY).replace("\'","\"")
-    mqttClient.publish("users_pos_ini", str(userPos_XY))
+    mqttClient.publish('users_pos_ini', str(userPos_XY),qos=1)
+    print('published')
+    mqttClient.loop_stop()
 
-def save_Q_table_mqtt(table, SINR, initial_real_reword, action, dronePos, episode, step, drone, topic_name = 'Q_table_collection.json', host='broker.hivemq.com', port=1883):
-    mqttClient=mqtt.Client()
+
+def save_Q_table_mqtt(table, SINR, initial_real_reword, lambdaVar,action, dronePos, episode, step, drone,
+                      topic_name = args.q_table_topic, host=args.mqttBroker, port=args.port):
+    mqttClient = mqtt.Client()
     mqttClient.on_connect = on_connect
     mqttClient.connect(host, port)
     data = {}
-    data['episode']=episode
+    data['episode'] = episode
     data['step'] = step
-    data['drone_number']=drone
-    data['agent']='drone'
-    drone_dict = data ['qtable'] = {}
+    data['drone_number'] = drone
+    data['lambda'] = lambdaVar
+    data['agent'] = 'drone'
+    drone_dict = data['qtable'] = {}
     for i in table[int(drone)].index:
         drone_dict['position: ' + i] = {}
         for j in table[int(drone)].columns:
             drone_dict['position: ' + i][j] = table[int(drone)].loc[i, j]
-    drone_dict['SINR'] = generate_dict_from_array( SINR, 'user')
+    drone_dict['SINR'] = generate_dict_from_array(SINR, 'user')
     drone_dict['state'] = generate_dict_from_array(dronePos, 'drone')
     drone_dict['action'] = action
     drone_dict['reward'] = initial_real_reword
     mqttClient.publish(topic_name, str(data))
 
-def save_initial_settling(U_p, D_p, name = args.database_name, collection_name ='initial_setting', host='localhost', port=27017):
-    myclient = pymongo.MongoClient(host='localhost', port=27017)
-    mydb = myclient[name]
-    dblist = myclient.list_database_names()
-    collection = mydb[collection_name]
-    initial_info = {}
-    initial_info ['random_seed'] = args.random_seed
-    initial_info ['num_drones'] = args.numDrones
-    initial_info ['num_users'] = args.numUsers
-    initial_info ['user_positions'] = generate_dict_from_array(U_p, "user")
-    initial_info ['drone_positions'] = generate_dict_from_array(D_p, 'drone')
-    initial_info ['carrier_frequency'] = args.fc
-    initial_info ['transmit_power'] = args.Pt
-    initial_info ['sinr_threshold'] = args.connectThresh
-    initial_info ['drone_user_capacity'] = 'not consider yet'
-    initial_info ['x_min'] = 0
-    initial_info ['x_max'] = args.width
-    initial_info ['y_min'] = 0
-    initial_info ['y_max'] = args.length
-    initial_info ['possible_actions'] = [[1,0],[-1,0],[0,1],[0,-1],[0,0]]
-    initial_info ['learning_rate'] = args.ALPHA
-    initial_info ['total_episodes'] = args.episode
-    initial_info ['iterations_per_episode'] = args.step
-    initial_info ['discount_factor'] = args.LAMBDA
-    initial_info ['episodes'] = 'total if possible'
-    result = collection.insert(initial_info)
-
-def save_Q_table(table, SINR, initial_real_reword, action, dronePos, episode, step, drone, name , collection_name, host='localhost', port=27017):
-    myclient = pymongo.MongoClient(host='localhost', port=27017)
-    mydb = myclient[name]
-    dblist = myclient.list_database_names()
-    data = {}
-    epoch_dict = data['episode: ' + episode] = {}
-    step_dict = epoch_dict['step: ' + step] = {}
-    drone_dict = step_dict ['drone number: ' + drone] = {}
-    for i in table[int(drone)].index:
-        drone_dict['position: ' + i] = {}
-        for j in table[int(drone)].columns:
-            drone_dict['position: ' + i][j] = table[int(drone)].loc[i, j]
-    drone_dict['SINR'] = generate_dict_from_array( SINR, 'user')
-    drone_dict['state'] = generate_dict_from_array(dronePos, 'drone')
-    drone_dict['action'] = action
-    drone_dict['reword'] = initial_real_reword
-    collection = mydb[collection_name]
-    result = collection.insert(data)
-
 def theoretical_performance(userPos, num):
     max = 0
     average = 0
-    for i in range (num):
+    for i in range(num):
         np.random.seed(i)
         drone_Pos = np.zeros((args.numDrones, 3))
         drone_Pos[:, 0:2] = np.random.randint(0, int(args.length / args.resolution), [args.numDrones, 2]) * 10 + 5
         drone_Pos[:, 2] = 30
-        _, _, initial_real_reword = models.alloc_users(userPos, drone_Pos, args.fc, args.dAngle, args.N0, args.BW, args.Pt,args.connectThresh)
+        _, _, initial_real_reword = models.alloc_users(userPos, drone_Pos, args.fc, args.dAngle, args.N0, args.BW,
+                                                       args.Pt, args.connectThresh)
         average += initial_real_reword
-        if max<= initial_real_reword:
+        if max <= initial_real_reword:
             max = initial_real_reword
     average /= num
     print('theoretical maximum performance', max, 'theoretical average performance', average)
     return max, average
 
+
 def main(args):
-    #refreash_dataset()
-
+    # refreash_dataset()
+    Lambda = args.LAMBDA
     dronePos, userPos, Q_table = environment_setup()
-    #max, average = theoretical_performance(userPos, 100000)
-
+    # max, average = theoretical_performance(userPos, 100000)
 
     count = []
     for i in range(args.episode):
-        dronePos = np.array([[0, 0, 30], [99, 99, 30]])
+        #dronePos = np.array([[0, 0, 30], [99, 99, 30]])
         total = 0
         counter = 0
         reword_table = np.zeros((args.numDrones, args.step))
@@ -295,28 +266,29 @@ def main(args):
             for k in range(args.numDrones):
                 initial_table_reword, initial_action, Q_table[k] = Q.choose_action(dronePos[k][:2], Q_table[k])
                 dronePos[k][:2] = Q.take_action(dronePos[k][:2], initial_action)
-                _, _, initial_real_reword = models.alloc_users(userPos,dronePos,args.fc,args.dAngle,args.N0,args.BW,args.Pt,args.connectThresh)
+                _, _, initial_real_reword = models.alloc_users(userPos, dronePos, args.fc, args.dAngle, args.N0,
+                                                               args.BW, args.Pt, args.connectThresh)
                 second_state = dronePos
-                second_table_reword , action, Q_table[k] = Q.choose_max_action(dronePos[k][:2], Q_table[k])
-                allocVec, SINR, second_real_reword = models.alloc_users(userPos,dronePos,args.fc,args.dAngle,args.N0,args.BW,args.Pt,args.connectThresh)
-                dronePos = second_state%args.length
-                if j<=args.step*3/4 or j>args.step*4/5:
-                    Q_table[k] = Q.update_Q_table(Q_table[k], initial_state[k][:2], initial_action, initial_table_reword, second_state[k][:2], second_table_reword, second_real_reword['total'])
+                second_table_reword, action, Q_table[k] = Q.choose_max_action(dronePos[k][:2], Q_table[k])
+                allocVec, SINR, second_real_reword = models.alloc_users(userPos, dronePos, args.fc, args.dAngle,
+                                                                        args.N0, args.BW, args.Pt, args.connectThresh)
+                dronePos = second_state % args.length
+                if j <= args.step * 3 / 4 or j > args.step * 4 / 5:
+                    Q_table[k] = Q.update_Q_table(Q_table[k], initial_state[k][:2], initial_action,
+                                                  initial_table_reword, second_state[k][:2], second_table_reword,
+                                                  second_real_reword['total'])
                 rewords = initial_real_reword['total']
-                #save_Q_table(Q_table, SINR, initial_real_reword, action, dronePos, str(i),str(j), str(k), args.database_name, args.collection_name)
-                save_Q_table_mqtt(Q_table, SINR, initial_real_reword, action, dronePos, i,j, k)
+                #ßsave_Q_table_mqtt(Q_table, SINR, initial_real_reword, Lambda,action, dronePos, i, j, k)
             counter += 1
             total += initial_real_reword['total']
-            if j%200 ==0:
-                print('episode', i,' with average reward:', total/counter)
-            reword_table[k,j] = rewords
-        count += [total/counter]
-       # np.save('Log/reword_episod_' + str(i)+ '.npy', count)
-        print (count)
-        print(Q_table)
+            if j % 200 == 0:
+                print('episode', i, ' with average reward:', total / counter)
+            reword_table[k, j] = rewords
+        count += [total / counter]
+        print(count)
+        #print(Q_table)
         print(dronePos)
-
+    np.save('results_qlearning_4drones.npy', count)
 
 if __name__ == "__main__":
     main(args)
-
